823e3ab76aca338e08fc94f26b7df217328d638d
johnsokk <johnsokk@rose-hulman.edu>
2020-04-03 10:05:20 -0400

    Hi

--- Ticket Organization Program/src/ticketPackage/Main.java
+++ Ticket Organization Program/src/ticketPackage/Main.java
@@ -5,13 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hiiiiiiiiiiiiii");
-<<<<<<< HEAD
+
 System.out.println("Konnichiwaaaaaa");
 System.out.println("Holaaaaaaaaaaaa");
-               
 
-=======
->>>>>>> branch 'master' of https://github.com/johnsokk/johnsokk-rose-hulman.edu
 	}
 
 }
