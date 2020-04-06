4fce52a4ad88ca5c41751eeee5e8be59e4ff0547
baileykc <baileykc@rose-hulman.edu>
2020-04-06 10:13:54 -0400

    AOC

--- Ticket Organization Program/src/ticketPackage/Main.java
+++ Ticket Organization Program/src/ticketPackage/Main.java
@@ -7,15 +7,12 @@ public class Main {
 System.out.println("Hiiiiiiiiiiiiii");
 System.out.println("Konnichiwaaaaaa");
 System.out.println("Holaaaaaaaaaaaa");
-<<<<<<< HEAD
 System.out.println("Braden's edit");
 System.out.println("I want the weekend to start!");
 System.out.println("sad");
-=======
 System.out.println("Bradens edit");
 System.out.println("I long for deathhhhhhhhhhhhhhhhh");
-
->>>>>>> branch 'master' of https://github.com/johnsokk/johnsokk-rose-hulman.edu
+System.out.println("I love Bernie Sanders!");
 	}
 
 }
