c4ce199d3fae3c7804872363bf47422689c5977c
baileykc <baileykc@rose-hulman.edu>
2020-04-06 11:23:09 -0400

    Day 1

--- Ticket Organization Program/src/ticketPackage/Main.java
+++ Ticket Organization Program/src/ticketPackage/Main.java
@@ -1,18 +1,60 @@
 package ticketPackage;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
 public class Main {
+	private JLabel label = new JLabel();
+	
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Hiiiiiiiiiiiiii");
-System.out.println("Konnichiwaaaaaa");
-System.out.println("Holaaaaaaaaaaaa");
-System.out.println("Braden's edit");
-System.out.println("I want the weekend to start!");
-System.out.println("sad");
-System.out.println("Bradens edit");
-System.out.println("I long for deathhhhhhhhhhhhhhhhh");
-System.out.println("I love Bernie Sanders!");
+		new Main();
 	}
+	public Main() {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		frame.setTitle("Welcome!");
+		frame.add(label, BorderLayout.CENTER);
+		frame.add(panel, BorderLayout.SOUTH); 
+		
+		JButton ticket = new JButton("Add Ticket");
+		label.setText("Select the 'Add Ticket' button below to "
+				+ "add a ticket or card to your "
+				+ "travel hub.");
+		panel.add(ticket);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	public class MainListener implements ActionListener{ 
+		public void actionPerformed(ActionEvent e) {
+			String ticketName = JOptionPane.showInputDialog("Enter a name for the ticket/card");
+			String ticketCost = JOptionPane.showInputDialog("What is the current balance on the ticket/card?")
+			int nTickets = Integer.parseInt(ticketCost);
+			
+			
+			JFrame  myFrame = new JFrame();
+			JPanel mypanel = new JPanel();
+			myFrame.setTitle("Add Ticket Information");
+			myFrame.add(mypanel, BorderLayout.CENTER);
+			
+			myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			myFrame.pack();
+			myFrame.setVisible(true);
+			
+		}
 
+		
+	}
 }
+
+
