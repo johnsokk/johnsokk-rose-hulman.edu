b4794da7a798e12ea03ecece0fa70b955a39ba75
baileykc <baileykc@rose-hulman.edu>
2020-04-16 10:38:36 -0400

    yay

--- Ticket Organization Program/src/ticketPackage/Main.java
+++ Ticket Organization Program/src/ticketPackage/Main.java
@@ -38,9 +38,8 @@ public class Main {
 	public class MainListener implements ActionListener{ 
 		public void actionPerformed(ActionEvent e) {
 			String ticketName = JOptionPane.showInputDialog("Enter a name for the ticket/card");
-			String ticketCost = JOptionPane.showInputDialog("What is the current balance on the ticket/card?")
+			String ticketCost = JOptionPane.showInputDialog("What is the current balance on the ticket/card?");
 			int nTickets = Integer.parseInt(ticketCost);
-			
 			
 			JFrame  myFrame = new JFrame();
 			JPanel mypanel = new JPanel();
