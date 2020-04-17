12e452117eccdb4cbef18e1c8fcac68b72303596
johnsokk <johnsokk@rose-hulman.edu>
2020-04-16 22:51:13 -0400

    This was one long night

--- Ticket Organization Program/src/ticketPackage/Main.java
+++ Ticket Organization Program/src/ticketPackage/Main.java
@@ -11,58 +11,62 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Main {
-	private JLabel label = new JLabel();
+	//private JLabel label = new JLabel();
 	
-	
-
 	public static void main(String[] args) {
-		new Main();
-	}
-	public Main() {
-		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
-		frame.setTitle("Welcome!");
-		frame.add(label, BorderLayout.CENTER);
-		frame.add(panel, BorderLayout.SOUTH); 
 		
-		JButton ticket = new JButton("Add Card");
+		AddCardJFrame AddCardJFrameClass = new AddCardJFrame();
+		AddCardJFrameClass.getClass();
+		//new Main();
+	//}
+	//public Main() {
+	//	JFrame frame = new JFrame();
+	//	JPanel panel = new JPanel();
+	//	frame.setTitle("Welcome!");
+	//	frame.add(label, BorderLayout.CENTER);
+	//	frame.add(panel, BorderLayout.SOUTH); 
 		
-		label.setText("Select the 'Add Card' button below to "
-				+ "add a card to your "
-				+ "travel hub.");
-		panel.add(ticket);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.pack();
-		frame.setVisible(true);
+	//	JButton ticket = new JButton("Add Card");
 		
-		//JButton myButton = new JButton();
-		//panel.add(myButton);
-	}
+	//	label.setText("Select the 'Add Card' button below to "
+	//			+ "add a card to your "
+	//			+ "travel hub.");
+	//	panel.add(ticket);
+	/*
+	 * frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); frame.pack();
+	 * frame.setVisible(true);
+	 * 
+	 * //JButton myButton = new JButton(); //panel.add(myButton); }
+	 * 
+	 * public class MainListener implements ActionListener{ public void
+	 * actionPerformed(ActionEvent e) {
+	 * 
+	 * String ticketName =
+	 * JOptionPane.showInputDialog("Enter a name for the ticket/card"); String
+	 * ticketCost =
+	 * JOptionPane.showInputDialog("What is the current balance on the ticket/card?"
+	 * ); int nTickets = Integer.parseInt(ticketCost);
+	 * 
+	 * 
+	 * JFrame myFrame = new JFrame(); JPanel mypanel = new JPanel();
+	 * myFrame.setTitle("Add Ticket Information"); myFrame.add(mypanel,
+	 * BorderLayout.CENTER);
+	 * 
+	 * myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); myFrame.pack();
+	 * myFrame.setVisible(true);
+	 * 
+	 * }
+	 * 
+	 */ 
 	
-	public class MainListener implements ActionListener{ 
-		public void actionPerformed(ActionEvent e) {
-			
-			String ticketName = JOptionPane.showInputDialog("Enter a name for the ticket/card");
-			String ticketCost = JOptionPane.showInputDialog("What is the current balance on the ticket/card?");
-			int nTickets = Integer.parseInt(ticketCost);
-			
-			
-			JFrame  myFrame = new JFrame();
-			JPanel mypanel = new JPanel();
-			myFrame.setTitle("Add Ticket Information");
-			myFrame.add(mypanel, BorderLayout.CENTER);
-			
-			myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			myFrame.pack();
-			myFrame.setVisible(true);
-			
-			// Close your tabs!!!!!!!!!!!!!!!
-			//Hi
-			
-		}
+	 }
 
+	private static void AddCardJFrame() {
+		// TODO Auto-generated method stub
 		
 	}
+	 
+	
 }
 
 
