f01bd8ff4a39545e0d335b1ad66702a6796191e3
smithbm2 <smithbm2@rose-hulman.edu>
2020-05-03 20:42:40 -0400

    *Noice

--- Ticket Organization Program/src/ticketPackage/HomePage.java
+++ Ticket Organization Program/src/ticketPackage/HomePage.java
@@ -1,38 +1,58 @@
 package ticketPackage;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class HomePage {
 	private HashMap<String, Integer> cardMap;
 	private JFrame homePage;
+	private JPanel cardPanel;
 
 	public HomePage() {
-
+		
+		cardPanel = new JPanel();
 		cardMap = new HashMap<String, Integer>();
 		homePage = new JFrame();
-		AddCardJFrame AddCardJFrameClass = new AddCardJFrame(cardMap, this);
+		//AddCardJFrame AddCardJFrameClass = new AddCardJFrame(cardMap, this);
+		GridLayout homePageLayout = new GridLayout(4, 1);
+		homePage.setLayout(homePageLayout);
 		JButton addCardButton = new JButton("Add Card Icon");
+		addCardButton.addActionListener(new AddCardListener(this.cardMap, this));
+		JLabel welcomeLabel = new JLabel();
+		welcomeLabel.setText("   Welcome! Select the 'Add Card Icon' button below to add a card to your card library.   ");
+		homePage.add(welcomeLabel);
+		
 		JPanel addCardPanel = new JPanel();
 		addCardPanel.add(addCardButton);
 		homePage.add(addCardPanel);
+		homePage.add(cardPanel);
 		homePage.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		homePage.setVisible(true);
 		homePage.pack();
-
+		
 	}
 
 	class AddCardListener implements ActionListener {
-		private HashMap cardMap;
+		private HashMap<String, Integer> cardMap;
 		private HomePage homePage;
+
+		public AddCardListener(HashMap cardMap, HomePage homePage) {
+			this.homePage = homePage;
+			this.cardMap = cardMap;
+		}
 		public void actionPerformed(ActionEvent e) {
-			//AddCardJFrame AddCardJFrameClass = new AddCardJFrame(cardMap, this.homePage);
+			AddCardJFrame AddCardJFrameClass = new AddCardJFrame(this.cardMap, this.homePage);
+			
+			//System.out.println("Check check");
 		}
 	}
 //public class MainListener implements ActionListener{ 
@@ -59,14 +79,15 @@ public class HomePage {
 	// }
 
 	public void displayCard() {
+		cardPanel.removeAll();
+		GridLayout cardLayout = new GridLayout(cardMap.size(), 1);
+		cardPanel.setLayout(cardLayout);
 		for (String key : cardMap.keySet()) {
-			System.out.println(key);
-			System.out.println(cardMap.get(key));
 			JButton tileCardButton = new JButton("<html>" + key + "<br/>" + "¥" + cardMap.get(key) + "</html>");
-			JPanel newPanel = new JPanel();
-			homePage.add(newPanel, BorderLayout.SOUTH);
-			newPanel.add(tileCardButton, BorderLayout.CENTER);
-
+			
+			cardPanel.add(tileCardButton);
+			//newPanel.add(tileCardButton);
+			//homePage.add(newPanel);
 			// System.out.println(cardMap.);
 		}
 		homePage.setVisible(true);
